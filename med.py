5cf1ecab4c6daaaa0a1f69719f70ef97da5096cc
andrei <che54606@gmail.com>
2021-07-21 17:23:29 +0300

    casi final

--- med.py
+++ med.py
@@ -1,5 +1,6 @@
 import openpyxl
 
+
 class Cases():
     def __init__(self, sheet, number):
         self.id = number
@@ -10,19 +11,21 @@ class Cases():
 def main():
     doc = openpyxl.load_workbook('med.xlsx')
     sheet = doc.get_sheet_by_name('Лист1')
+    sheet_active = doc.active
     rows = sheet.max_row
     compl_lst = []
     cases_lst = []
     for item in range(2, rows+1):
-        cases_lst.append(Cases(sheet, item))
-    print(len(cases_lst))
+        res7 = Cases(sheet, item)
+        sheet_active.cell(row=item, column=18).value = analyt(res7)
+    doc.save('med.xlsx')
 
 def analyt(obj):
     if obj.opernumber <= 924.5:
         if obj.opernumber <= 78.65:
             return 'нет'
         else:
-            if obj.age <= 75:
+            if obj.age <= 75.1:
                 if obj.opernumber <= 159:
                     return 'нет'
                 else:
@@ -33,7 +36,7 @@ def analyt(obj):
                             return 'нет'
                         else:
                             return 'есть'
-            else: return 'нет'
+            else: return 'есть'
     else:
         if obj.age <= 75:
             if obj.age <= 73:
@@ -65,3 +68,7 @@ def analyt(obj):
                                     return 'нет'
                                 else:
                                     return 'есть'
+        else:
+            if obj.mkb == 'старческая ядерная катаракта':
+                return 'нет'
+            else: return 'есть'
